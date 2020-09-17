9e01d61c059444b7a7feacb30e6ba828b17ed4c4
Latisha Joe <latisha_joe@yahoo.com>
2020-09-17 09:00:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(array)
+  new_array = []
+  array.sort do |a, b|
+    a <=> b
+ end
+end
 
+def sort_array_desc(array)
+  new_array = []
+  array.sort{|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  new_array = []
+  array.sort{|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  new_array = []
+  array.sort{|a, b| a[1] <=> b[2]}
+end
+
+def reverse_array(array)
+  new_array = []
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |a|
+   a[2] = "$"
+  end
+end
