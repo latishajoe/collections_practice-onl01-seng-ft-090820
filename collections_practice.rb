3ab3dc695283804ce808b7ba18d59023dddf2a42
Latisha Joe <latisha_joe@yahoo.com>
2020-09-17 09:38:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,3 +44,12 @@ def sum_array(array)
     sum + x
  end
 end
+
+def add_s(array)
+  array.each_with_index.collect{|word, index| }
+  if index = 1 
+    word = word + "s"
+  else 
+    word = word 
+   end 
+ end
